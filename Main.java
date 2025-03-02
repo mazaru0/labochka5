11072f21bf517f34e9f5c94ce8cb935a72739ad5
ZhenkaPechenka6 <zhenkapechenka06@gmail.com>
2025-03-02 12:13:34 +0300

    Минимальные настройки ( мэин и абстрактный класс)
    main и command сделала

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner (System.in);
+        HashMap<String, String> map = new HashMap<>();
+        while (in.hasNextLine()){
+            String line = in.nextLine();
+        }
+}}
